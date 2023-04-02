ec4ecbf51668270e3dcf1f6bc6e71e500af207c5
yunussyasar <yunusyasar.tr2114@gmail.com>
2023-04-03 01:21:30 +0300

    Heapsort unit test

---
+++ ce100-hw2-ebru-yunus/hw2-algo-test/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace hw2_algo_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestHeapSort_BestCase_1000Inputs()
+        {
+            // Arrange
+            int[] arr = Enumerable.Range(1, 1000).ToArray();
+
+            // Act
+            int[] sortedArr = HeapSort(arr);
+
+            // Assert
+            CollectionAssert.AreEqual(arr, sortedArr);
+        }
+
+        private int[] HeapSort(int[] arr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
